f9ebd39d076b93336ca55f7322e14b33e53673eb
flyingriverhorse <buzzycarl@gmail.com>
2025-12-15 09:02:17 +0200

    chore: remove unused test file and typecheck document

--- test_mypy_float.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from typing import Any, List
-
-def test():
-    result = pd.Series([1.0, 2.0])
-    constants: List[Any] = [1.0, 2.0]
-    for c in constants:
-        result = result - float(c)
-        result = result * float(c)
