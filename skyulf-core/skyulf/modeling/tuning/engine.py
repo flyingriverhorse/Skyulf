1b3b5645c344dd200273f1381f396f46d9307a18
flyingriverhorse <buzzycarl@gmail.com>
2026-04-24 21:05:01 +0300

    Refactor import statements to add type ignores for unresolved imports across multiple modules

--- skyulf-core/skyulf/modeling/tuning/engine.py
+++ skyulf-core/skyulf/modeling/tuning/engine.py
@@ -40,23 +40,23 @@ except ImportError:
 
 if HAS_OPTUNA:
     try:
-        from optuna.integration import (
+        from optuna.integration import (  # ty: ignore[unresolved-import]
             OptunaSearchCV as _OptunaSearchCV,
-        )  # ty: ignore[unresolved-import]
+        )
 
         OptunaSearchCV = _OptunaSearchCV
     except ImportError:
         try:
-            from optuna.integration.sklearn import (
+            from optuna.integration.sklearn import (  # ty: ignore[unresolved-import]
                 OptunaSearchCV as _OptunaSearchCV,
-            )  # ty: ignore[unresolved-import]
+            )
 
             OptunaSearchCV = _OptunaSearchCV
         except ImportError:
             try:
-                from optuna_integration.sklearn import (
+                from optuna_integration.sklearn import (  # ty: ignore[unresolved-import]
                     OptunaSearchCV as _OptunaSearchCV,
-                )  # ty: ignore[unresolved-import]
+                )
 
                 OptunaSearchCV = _OptunaSearchCV
             except ImportError:
