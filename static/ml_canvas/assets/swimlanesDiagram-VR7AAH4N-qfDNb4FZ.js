f65fdc4fa6a55c191640932f5b0fbd73d8a57a75
flyingriverhorse <flyingriverhorse@users.noreply.github.com>
2026-09-10 16:05:30 +0000

    deploy: 988b5a43d5a9ddb0b1027e8f5e8b154d75b6d26b

---
+++ static/ml_canvas/assets/swimlanesDiagram-VR7AAH4N-qfDNb4FZ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-DzaVugIQ.js";import{_ as a}from"./mermaid.core-C0M8Jm9J.js";import"./vendor-flow-Ct2sLZk5.js";import"./chunk-5VM5RSS4-DOn04oQD.js";import"./chunk-XXDRQBXY-1OFHVVaI.js";import"./chunk-POPQ4Y6H-Br6EHtru.js";import"./chunk-F27PBJKO-CAdY9bUS.js";import"./channel-sPrkgFbZ.js";import"./index-BVvggaqQ.js";import"./vendor-utils-VkF_O5b_.js";import"./vendor-charts-BkHP8M8k.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
